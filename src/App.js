7f74dc21a36037d0ae31815d968e62a32a688292
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-10 01:54:37 +0200

    Implement routing with 3 Nav options
    - Create NavBar module and initialise with styling
    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import CalculatorUI from './components/calculatorComponent/Calculator';
+import NavBar from './components/navBarComponent/NavBar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <CalculatorUI />
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<CalculatorUI />} />
+      </Routes>
+    </>
   );
 }
 
