097c740ba16841291048148b0774dd8b5b28e9d0
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-09 22:03:07 +0200

    Create react app to set up project
    Get rid of CRA boilerplate and initialise Calculator component with a js file and a  css file as a css module

---
+++ src/components/calculator-component/Calculator.js
@@ -0,0 +1,12 @@
+import styles from './Calculator.module.css';
+import React, { useState } from 'react'; 
+
+const CalculatorUi = () => {
+  return(
+    <>
+      <div> CAlculator Ui</div>
+    </>
+  )
+}
+
+export default CalculatorUi;
